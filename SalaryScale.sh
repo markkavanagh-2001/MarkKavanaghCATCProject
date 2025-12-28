f4aaef504a714fe6330129709dcbef40db1f8fe4
markkavanagh-2001 <markkavanagh2001@gmail.com>
2025-12-28 19:51:22 +0000

    Set up .sh file, user input lines and for loop

---
+++ SalaryScale.sh
@@ -0,0 +1,14 @@
+echo "Please enter your starting salary:"
+read startingSalary
+echo "Please enter how many years you wish to show your salary scale for:"
+read points
+echo "Please enter the salary increment that you expect:"
+read increment
+
+for ((i=1; i<=points; i++))
+do
+ echo "year $i:" +  $startingSalary + $i*$increment
+done
+
+	
+
