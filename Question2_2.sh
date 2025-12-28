397cc3138c496e5d1f86f75a5f5ffda1331383bf
markkavanagh-2001 <markkavanagh2001@gmail.com>
2025-12-28 18:09:37 +0000

    Added formatting to clean up the output of shell script

--- Question2_2.sh
+++ Question2_2.sh
@@ -1,16 +1,46 @@
 #! /bin/bash
 #This is Question 2.2:
-set -o history
 exec > MarkKavanagh.txt 2>&1
 
+#Enable history command
+set -o history
+
+echo "DATE COMMAND:"
 date
+
+echo "   "
+echo "HOSTNAME COMMAND:"
 hostname
+
+echo "   "
+echo "ARCH COMMAND:"
 arch
+
+echo "   "
+echo "UNAME -A COMMAND:"
 uname -a
+
+echo "   "
+echo "UPTIME COMMAND:"
 uptime
+
+echo "   "
+echo "WHOAMI COMMAND:"
 whoami
+
+echo  "    "
+echo "WHO COMMAND:"
 who
+
+echo "   "
+echo "W COMMAND:"
 w
+
+echo " "
+echo "TOP COMMAND:"
 top -n 1
+
+echo " "
+echo "HISTORY COMMAND:"
 history
 
