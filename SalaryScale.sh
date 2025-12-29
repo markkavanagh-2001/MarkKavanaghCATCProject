19b653aff0810361eb86d821b8b736edcf29434e
markkavanagh-2001 <markkavanagh2001@gmail.com>
2025-12-29 13:49:18 +0000

    Manager increments now working. Begin to comment code to explain its workings

--- SalaryScale.sh
+++ SalaryScale.sh
@@ -1,14 +1,17 @@
+#Question 2.4
+
+#First ask user the required salary questions, and if they are a manager
 read -p  "Please enter your starting salary:" startingSalary
 read -p  "Please enter how many years you wish to show your salary scale for:" points
 read -p  "Please enter the salary increment that you expect:" increment
 read -p "Are you a manager (Y/N):" manager
 
+#If the user is a manager, they will receive 2 pay rises per year
 if [[ "$manager" = "Y" || "$manager" = "y" ]]; then
-	echo "year: 0 " $startingSalary
-	for ((i=1; i<=($points * 2); i++))
+	#There will now be double the number of points to plot. 
+	for ((i=0; i<=($points * 2); i++)) #Calculate the salary for each point
 	do
-		year=$(printf "%.2f" $($i/2))
-		echo "year: $year"  $(( $startingSalary + ($i * $increment)))
+		echo "year: $(echo "scale=1; $i/2" | bc )"  $(( $startingSalary + ($i * $increment)))
 	done
 elif [[ "$manager" = "N" || "$manager" = "n" ]]; then
 	echo "IncsPerYear = 1"
