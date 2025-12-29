181c23fc3f01d8318a47d51b3d9dd72f837e0b22
markkavanagh-2001 <markkavanagh2001@gmail.com>
2025-12-29 14:14:30 +0000

    Commented code

--- SalaryScale.sh
+++ SalaryScale.sh
@@ -8,28 +8,21 @@ read -p "Are you a manager (Y/N):" manager
 
 #If the user is a manager, they will receive 2 pay rises per year
 if [[ "$manager" = "Y" || "$manager" = "y" ]]; then
-	#There will now be double the number of points to plot. 
-	for ((i=0; i<=($points * 2); i++)) #Calculate the salary for each point
+	for ((i=0; i<=($points * 2); i++))
 	do
-		yearDecimal=$(echo "scale=1; $i/2" | bc)
-		salaryIncrement=$(($startingSalary + ($i * $increment)))
-		echo "year: $yearDecimal ------ Salary: " $salaryIncrement  
+		yearDecimal=$(echo "scale=1; $i/2" | bc) #Calaculate the year value for each increment and store as a decimal (i.e 0, 0.5, 1.5 ....).
+		salaryIncrement=$(($startingSalary + ($i * $increment))) #Calculate the salary at each increment.
+		echo "year: $yearDecimal ------ Salary: " $salaryIncrement  #Print the year and corresponding salary value.
 	done
-elif [[ "$manager" = "N" || "$manager" = "n" ]]; then
+elif [[ "$manager" = "N" || "$manager" = "n" ]]; then #if user is regular employee, there will only be 1 raise per year
 	for ((i=0; i<=points; i++))
 	do
-		salaryIncrement=$(($startingSalary + ($i * $increment)))
-		echo "year: $i ------ Salary: " $salaryIncrement
+		salaryIncrement=$(($startingSalary + ($i * $increment))) #Calculate the salary at each increment and store in salaryIncrement variable
+		echo "year: $i ------ Salary: " $salaryIncrement #Print year and corresponding salary value
 	done
 else
-	echo "Please enter 'Y', 'y', 'N', or 'n'"
+	echo "Please enter 'Y', 'y', 'N', or 'n'" #If user inputs anything other than y/n, instruct them to input the correct command.
 fi
-
-
-#for ((i=0; i<=points; i++))
-#do
-#	echo "year $i:" $(( $startingSalary + ($i * $increment)))
-#done
 
 
 
