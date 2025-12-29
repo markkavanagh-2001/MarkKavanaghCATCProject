48018e7e61a7093817420160cbb784a2dc4a6a02
markkavanagh-2001 <markkavanagh2001@gmail.com>
2025-12-29 12:28:52 +0000

    Added prompt to check if user is manager and added if statement with test output for manager condition

--- SalaryScale.sh
+++ SalaryScale.sh
@@ -4,11 +4,24 @@ echo "Please enter how many years you wish to show your salary scale for:"
 read points
 echo "Please enter the salary increment that you expect:"
 read increment
+read -p "Are you a manager (Y/N):" manager
 
-for ((i=1; i<=points; i++))
+
+if [[ "$manager" = "Y" || "$manager" = "y" ]]; then
+	echo "IncsPerYear = 2"
+else
+	echo "Please enter 'Y', 'y', 'N', or 'n'"
+fi
+
+
+echo "$IncsPerYear"
+
+for ((i=0; i<=points; i++))
 do
 	echo "year $i:" $(( $startingSalary + ($i * $increment)))
 done
+
+
 
 	
 
